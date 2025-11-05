8294b3be1c4b34247eaad9b668d2b3e6b61b1f53
enrico hu <enricomc11@gmail.com>
2025-11-05 21:13:34 +0100

    Add user validation and JSON storage support for accounts and placemarks.

--- src/models/db.js
+++ src/models/db.js
@@ -1,12 +1,25 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { placemarkMemStore } from "./mem/placemark-mem-store.js";
+import { userJsonStore } from "./json/user-json-store.js";
+import { placemarkJsonStore } from "./json/placemark-json-store.js";
 
 export const db = {
   userStore: null,
   placemarkStore: null,
 
-  init() {
-    this.userStore = userMemStore;
-    this.placemarkStore = placemarkMemStore;
+  init(storeType) {
+    switch (storeType) {
+      case "mem":
+        this.userStore = userMemStore;
+        this.placemarkStore = placemarkMemStore;
+        break;
+      case "json":
+        this.userStore = userJsonStore;
+        this.placemarkStore = placemarkJsonStore;
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.placemarkStore = placemarkMemStore;
+    }
   },
 };
---
+++ src/models/json/placemark-json-store.js
@@ -0,0 +1,40 @@
+import { v4 } from "uuid";
+import { db } from "./store-utils.js";
+
+export const placemarkJsonStore = {
+  async getAllPlacemarks() {
+    await db.read();
+    return db.data.placemarks;
+  },
+
+  async addPlacemark(placemark) {
+    await db.read();
+    placemark._id = v4();
+    db.data.placemarks.push(placemark);
+    await db.write();
+    return placemark;
+  },
+
+  async getPlacemarkById(id) {
+    await db.read();
+    const list = db.data.placemarks.find((placemark) => placemark._id === id);
+    return list;
+  },
+
+  async getUserPlacemarks(userid) {
+    await db.read();
+    return db.data.placemarks.filter((placemark) => placemark.userid === userid);
+  },
+
+  async deletePlacemarkById(id) {
+    await db.read();
+    const index = db.data.placemarks.findIndex((placemark) => placemark._id === id);
+    db.data.placemarks.splice(index, 1);
+    await db.write();
+  },
+
+  async deleteAllPlacemarks() {
+    db.data.placemark = [];
+    await db.write();
+  },
+};
